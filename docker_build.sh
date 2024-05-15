4816e101cf4972141b43db3f3d78646473c41b49
nandhu <nandhu.elan@gmail.com>
2024-05-15 22:59:02 +0530

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,2 @@
-docker build -t harbor.hpc.ford.com/enandhag/docai:v1 \
-             --build-arg https_proxy=http://internet.ford.com:83/ \
-             --build-arg http_proxy=http://internet.ford.com:83/ \
-             --build-arg no_proxy=.ford.com,localhost,127.0.0.1 \
+docker build -t test_docai:v1 \
              -f Dockerfile .
